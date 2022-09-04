c4f4e3d156bffa019a8d649ffd239c7ecc35676c
miwanoff <marina.iva.noffa@gmail.com>
2022-09-04 14:05:59 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+console.log("JavaScript");
+let a = prompt("Input a");
+let b = prompt("Input b");
+let c = prompt("Input c");
+let x;
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  alert("Not a Number!");
+} else {
+  if (a == 0) {
+    if (b != 0) {
+      x = -c / b;
+      alert(x);
+    } else {
+      alert("Error");
+    }
+  } else {
+    let d = b * b - 4 * a * c;
+    if (d < 0) {
+      /* todo*/
+    } else if (d === 0) {
+      /* todo*/
+    } else {
+      /* todo*/
+    }
+  }
+}
---
+++ sweets.js
@@ -0,0 +1,14 @@
+"use strict";
+let count = prompt("How many candies?");
+while(/*todo */){
+    
+    let q = confirm ("Ще цукерку?");
+    if (q == true){
+        count--;
+        /* todo */
+    }
+    else {
+        break;
+        /* todo */
+    }
+}
